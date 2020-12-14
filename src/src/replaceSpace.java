a0cce35f1b1db4dba3a92097151fc13d0cbbc9d9
NuradinAhmed <Nuradinahmed2012@gmail.com>
2020-12-14 12:23:59 -0600

    string manipulation using character array

--- src/src/replaceSpace.java
+++ src/src/replaceSpace.java
@@ -1,53 +1,42 @@
 import java.util.Arrays;
 
 public class replaceSpace {
-
-
-
-    //create a method for the space replacement
     public static void replaceSpaces(char[] str, int trueLength) {
-        int numberOfSpaces = countOfChar(str, 0, trueLength, ' ');
-        int newIndex = trueLength - 1 + numberOfSpaces * 2;
+        int spaceCount = 0;
+        int index;
+
+        for (int i = 0; i < trueLength; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
 
 
-        //if there are excess spaces, add a null character. This indicates that the spaces after that point have not been replaced with %20.
-        if (newIndex + 1 < str.length)
-            str[newIndex + 1] = '\0';
-
-        for (int oldIndex = trueLength - 1; oldIndex >= 0; oldIndex = oldIndex - 1){ //oldIndex -= 1
-            if(str[oldIndex] == ' ') { //Insert %20
-                str[newIndex] = '0';
-                str[newIndex - 1] = '2';
-                str[newIndex - 2] = '%';
-                newIndex = newIndex - 3;
-
+        index = trueLength + spaceCount * 2;
+        if (trueLength < str.length)
+            str[trueLength] = '\0';
+        for (int i = trueLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
             } else {
-                str[newIndex] = str[oldIndex];
-                newIndex = newIndex - 1;
+                str[index - 1] = str[i];
+                index--;
             }
         }
 
+        System.out.println(str);
     }
 
 
-    //Count occurrences of target between start (inclusive) and end (exclusive).
-    static int countOfChar(char[] str, int start, int end, int target) {
-        int count = 0;
-        for(int i = start; i < end; i ++) {
-            if(str[i] == target) {
-                count++;
-            }
-        }
-        return count;
-    }
 
+    public static void main(String[] args) {
+        String str = "Mr John Smith                ";
+        replaceSpaces(str.toCharArray(), str.trim().length()); //so this str.trim().length() first trims spaces at the end and
+                                                                // then gets the length count for truelength;
 
-    public  static void main(String[] args) {
-        String str = "Mr John Smith  "; //created string and then convert to an array of character
-        char[] arr = str.toCharArray();
-        int trueLength = countOfChar(arr) + 1;
-        replaceSpaces(arr, trueLength);
-        System.out.println("\"" + AssortedMethods.char);
-
+      //  System.out.println(str);
     }
 }
