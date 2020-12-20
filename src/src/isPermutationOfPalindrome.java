7c9e4568770bb2d0f33c55d7a20d9cae2c562e3a
NuradinAhmed <Nuradinahmed2012@gmail.com>
2020-12-20 06:37:00 +0300

    string manipulation using character array

--- src/src/isPermutationOfPalindrome.java
+++ src/src/isPermutationOfPalindrome.java
@@ -1,2 +1,13 @@
-public class palindromePermutation {
+public class isPermutationOfPalindrome {
+
+    /*Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phase that is the same
+    forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+
+    Qustions: What does it take to be able to write a set of characters the same way forwards and backwards ? we need to have an even number
+                of almost all characters, so that half can be on one side and half can be on the other side. At most one character(the middle character)
+                can have an odd count.
+
+                for example. we know tactcoapapa is a permutation of a palindrome because it has two Ts, four As, two Cs, two Ps, and one O.
+                that O would be the center of all possible palindrome.
+     */
 }
