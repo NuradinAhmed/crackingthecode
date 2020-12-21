c876f12f710244ccb1fe57b8ef0ded3a817eb60d
NuradinAhmed <Nuradinahmed2012@gmail.com>
2020-12-21 08:08:54 +0300

    string manipulation using character array

--- src/src/sumList.java
+++ src/src/sumList.java
@@ -1,11 +1,13 @@
 import packages.PartialSum;
 import packages.packages.LinkedListNode;
 
-
+//Q: You have two numbers represented by a linkedlist, where each node contains a single digit. the digits are stored in
+/// reverse orde
 
 public class sumList {
 
     public static class partialSum {
+
         private static int length(LinkedListNode l) {
             if (l == null) {
                 return 0;
@@ -14,28 +16,30 @@ public class sumList {
             }
         }
 
-         static PartialSum addListsHelper(LinkedListNode l1, LinkedListNode l2) {
-            if (l1 == null && l2 == null) {
-                PartialSum sum = new PartialSum();
-                return sum;
-            }
-            PartialSum sum = addListsHelper(l1.next, l2.next);
-            int val = sum.carry + l1.data + l2.data;
-            LinkedListNode full_result = insertBefore(sum.sum, val % 10);
-            sum.sum = full_result;
-            sum.carry = val / 10;
-            return sum;
-        }
+
 
         private static LinkedListNode addLists(LinkedListNode l1, LinkedListNode l2) {
             int len1 = length(l1);
             int len2 = length(l2);
+
+
+            /* Pad the shorter list with zeros - see note (1) - one list may be shorter than the other. we can accomplish this by
+                comparing the lengths of the list in the beginning and padding the shorter list with zeros'
+             */
             if (len1 < len2) {
                 l1 = padList(l1, len2 - len1);
             } else {
                 l2 = padList(l2, len1 - len2);
             }
+
+
+            /* Add lists */
             PartialSum sum = addListsHelper(l1, l2);
+
+
+            /* if there was a carry value left over, insert this at the front of the list.
+                otherwise, just return the linked list.
+             */
             if (sum.carry == 0) {
                 return sum.sum;
             } else {
@@ -44,6 +48,32 @@ public class sumList {
             }
         }
 
+
+
+
+        static PartialSum addListsHelper(LinkedListNode l1, LinkedListNode l2) {
+            if (l1 == null && l2 == null) {
+                PartialSum sum = new PartialSum();
+                return sum;
+            }
+            /* Add smaller digits recursively */
+            PartialSum sum = addListsHelper(l1.next, l2.next);
+
+            /* Add carry to current data */
+            int val = sum.carry + l1.data + l2.data;
+
+            /* Insert sum of current digits */
+            LinkedListNode full_result = insertBefore(sum.sum, val % 10);
+
+            /* return sum so far, and the carry value */
+            sum.sum = full_result;
+            sum.carry = val / 10;
+            return sum;
+        }
+
+
+
+        /* Pad the list with zeros */
         private static LinkedListNode padList(LinkedListNode l, int padding) {
             LinkedListNode head = l;
             for (int i = 0; i < padding; i++) {
@@ -52,6 +82,10 @@ public class sumList {
             return head;
         }
 
+
+
+
+        /* Helper function to insert node in the front of a linked list */
         private static LinkedListNode insertBefore(LinkedListNode list, int data) {
             LinkedListNode node = new LinkedListNode(data);
             if (list != null) {
@@ -59,6 +93,10 @@ public class sumList {
             }
             return node;
         }
+
+
+
+
 
         public static int linkedListToInt(LinkedListNode node) {
             int value = 0;
@@ -68,6 +106,10 @@ public class sumList {
             }
             return value;
         }
+
+
+
+
 
         public static void main(String[] args) {
             LinkedListNode lA1 = new LinkedListNode(3, null, null);
@@ -88,7 +130,7 @@ public class sumList {
             int l3 = linkedListToInt(list3);
 
             System.out.print(l1 + " + " + l2 + " = " + l3 + "\n");
-            System.out.print(l1 + " + " + l2 + " = " + (l1 + l2));
+           // System.out.print(l1 + " + " + l2 + " = " + (l1 + l2));
         }
     }
 }
