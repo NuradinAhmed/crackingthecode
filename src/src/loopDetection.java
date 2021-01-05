46cedfa3ed6fdbc151b8c7ba5d5dffce9dc9eba2
NuradinAhmed <Nuradinahmed2012@gmail.com>
2021-01-05 16:12:53 +0300

    checking the intersection of two linked list nodes

--- src/src/loopDetection.java
+++ src/src/loopDetection.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+/*
+    Loop Detection: Given a linked list which might contain a loop, implement an algorithm that returns the node
+                    at the beginning of the loop (if exists).\
+                    Ex: Input: A > B > C > D > E > C [the same C as earlier]
+                        Output: C
+ */
+
 
 public class loopDetection {
 }
